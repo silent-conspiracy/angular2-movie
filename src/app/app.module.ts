1b1a716a217f6264443b997dbbd6dced8964be1e
Keng Wei <keng_wei2000@yahoo.com>
2018-01-09 01:17:48 +0800

    Simple Angular Router usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GridViewComponent } from './grid-view/grid-view.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieService } from './movie.service';
 
+const appRoutes: Routes = [
+  {path: 'movies', component: GridViewComponent},
+  {path: '', redirectTo: '/movies', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +21,8 @@ import { MovieService } from './movie.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [MovieService],
   bootstrap: [AppComponent]
