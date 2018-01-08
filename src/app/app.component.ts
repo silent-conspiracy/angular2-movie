b3f71bc6658efd4673674dfabc245c15c32facea
Keng Wei <keng_wei2000@yahoo.com>
2018-01-08 22:58:52 +0800

    Event binding for keyup on searchInput template variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  search = "";
+
+  updateSearch(value: string) {
+    this.search = value;
+    console.log(this.search);
+  }
 }
